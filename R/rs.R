83118e44a7a0947138f9806f8d409ac0d331f14d
ALIMALII <alifadhaa16@gmail.com>
2024-12-01 11:54:48 +0300

    Add files via upload

---
+++ R/rs.R
@@ -0,0 +1,16 @@
+#' This is package
+#' @export
+#' @param n Numerical Variable
+#' @param nv Numerical Variable
+
+
+rs<-function(n,nv){
+  X<-c();b<-c()
+  for (i in 1:nv){
+    b<-cbind(b,sample(seq(2,7),1))
+    X<-cbind(X,rnorm(n,0,1))
+  }
+  XX<-as.matrix(X)
+  Y<-XX%*%t(b)
+  YX<-cbind(Y,XX)
+}
